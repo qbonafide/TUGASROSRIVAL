5f1944031b863f40e8851157f887641b8062bbfb
qbonafide <christinatan504@gmail.com>
2024-10-24 17:22:27 +0700

    Update tina.cpp

--- turtleism_tina/src/tina.cpp
+++ turtleism_tina/src/tina.cpp
@@ -1,97 +1,177 @@
 #include "ros/ros.h"
-#include "geometry_msgs/Twist.h"
-#include "turtlesim/TeleportAbsolute.h"
 #include "turtlesim/SetPen.h"
+#include "geometry_msgs/Twist.h"
+#include "turtlesim/Pose.h"
+#include <cmath>
 
-void setPen(bool is_down) {
+using namespace std;
+
+ros::Publisher velocity_publisher;
+ros::Subscriber pose_subscriber;
+turtlesim::Pose turtlesim_pose;
+
+const double Pi = 3.14159265359;
+const double linear_speed = 2.0;
+
+void setPen(bool is_down, int width = 3, int r = 255, int g = 20, int b = 147) {
     turtlesim::SetPen srv;
-    srv.request.off = !is_down; // true = tidak gambar
+    srv.request.width = width;   
+    srv.request.r = r;          
+    srv.request.g = g;         
+    srv.request.b = b; 
+
+    srv.request.off = !is_down;
     if (!ros::service::call("/turtle1/set_pen", srv)) {
         ROS_ERROR("Failed to call service set_pen");
     }
 }
 
-void moveTurtle(ros::Publisher pub, double linear, double angular, double duration) {
-    geometry_msgs::Twist move_cmd;
-    move_cmd.linear.x = linear;
-    move_cmd.angular.z = angular;
-
-    ros::Rate rate(10); // 10 Hz
-    int ticks = duration * 10;
-    for (int i = 0; i < ticks; i++) {
-        pub.publish(move_cmd);
-        rate.sleep();
-    }
+double degrees2radians(double angle_in_degrees) {
+    return angle_in_degrees * Pi / 180.0;
 }
 
-void drawT(ros::Publisher pub) {
-    moveTurtle(pub, 2.0, 0.0, 1.0); 
-    moveTurtle(pub, 0.0, 1.57, 0.5); 
-    moveTurtle(pub, 1.0, 0.0, 1.0); 
-    moveTurtle(pub, 0.0, -1.57, 0.5); 
-    moveTurtle(pub, 2.0, 0.0, 1.0); 
+void move(double speed, double distance, bool isForward) {
+    geometry_msgs::Twist vel_msg;
+    vel_msg.linear.x = isForward ? speed : -speed;
+    vel_msg.angular.z = 0;
+
+    double t0 = ros::Time::now().toSec();
+    double current_distance = 0.0;
+    ros::Rate loop_rate(10);
+
+    do {
+        velocity_publisher.publish(vel_msg);
+        double t1 = ros::Time::now().toSec();
+        current_distance = speed * (t1 - t0);
+        ros::spinOnce();
+        loop_rate.sleep();
+    } while (current_distance < distance);
+
+    vel_msg.linear.x = 0;
+    velocity_publisher.publish(vel_msg);
 }
 
-void drawI(ros::Publisher pub) {
-    moveTurtle(pub, 0.0, 1.57, 0.5); 
-    moveTurtle(pub, 2.0, 0.0, 1.0); 
+void rotate(double angle_in_degrees, bool clockwise) {
+    geometry_msgs::Twist vel_msg;
+    double angular_speed = degrees2radians(60);
+
+    vel_msg.angular.z = clockwise ? -fabs(angular_speed) : fabs(angular_speed);
+
+    double angle_in_radians = degrees2radians(angle_in_degrees);
+    double t0 = ros::Time::now().toSec();
+    double current_angle = 0.0;
+    ros::Rate loop_rate(100);
+
+    do {
+        velocity_publisher.publish(vel_msg);
+        double t1 = ros::Time::now().toSec();
+        current_angle = angular_speed * (t1 - t0);
+        ros::spinOnce();
+        loop_rate.sleep();
+    } while (current_angle < angle_in_radians);
+
+    vel_msg.angular.z = 0;
+    velocity_publisher.publish(vel_msg);
 }
 
-void drawN(ros::Publisher pub) {
-    moveTurtle(pub, 2.0, 0.0, 1.0); 
-    moveTurtle(pub, 0.0, 1.57, 0.5); 
-    moveTurtle(pub, 2.5, 0.0, 1.5); 
-    moveTurtle(pub, 0.0, -1.57, 0.5); 
-    moveTurtle(pub, 2.0, 0.0, 1.0); 
+void drawT() {
+    setPen(true);  
+
+    move(linear_speed, 2.0, true);  
+    rotate(180, true);
+    move(linear_speed, 2.0, true);  
+    rotate(180, true);
+    move(linear_speed, 2.0, true);  
+    rotate(180, true);
+    move(linear_speed, 1.0, true); 
+    rotate(90, true);  
+    move(linear_speed, 2.0, true);
+
 }
 
-void drawA(ros::Publisher pub) {
-    moveTurtle(pub, 2.0, 1.0, 1.0); 
-    moveTurtle(pub, 2.0, -1.0, 1.0); 
-    moveTurtle(pub, 0.0, 3.14, 0.5); 
-    moveTurtle(pub, 1.0, 0.0, 0.5); 
+void drawI() {   
+    setPen(true);
+
+    move(linear_speed, 2.0, true);
+    rotate(180, true); 
+    move(linear_speed, 2.0, true); 
+    
 }
 
-void teleportTurtle(ros::ServiceClient teleport_client, float x, float y, float theta) {
-    turtlesim::TeleportAbsolute srv;
-    srv.request.x = x;
-    srv.request.y = y;
-    srv.request.theta = theta;
-    teleport_client.call(srv);
+void drawN() {
+
+    setPen(true); 
+
+    move(linear_speed, 2.0, true); 
+    rotate(135, true);
+    move(linear_speed, 2.5, true); 
+    rotate(135, false);
+    move(linear_speed, 2.0, true);
+    rotate(180, true);
+    move(linear_speed, 2.0, true); 
+
+}
+
+void drawA() {
+    setPen(true);
+
+    rotate(30, true);
+    move(linear_speed, 2.0, true);
+    rotate(120, true); 
+    move(linear_speed, 2.0, true); 
+    rotate(180, true);
+    move(linear_speed, 1.0, true); 
+    rotate(60, false); 
+    move(linear_speed, 1.0, true); 
+
+    setPen(false);
+}
+
+void executeDrawing() {
+    
+    setPen(false);
+    rotate(180, false);
+    move(linear_speed, 3.5, true);
+    drawT();
+
+    setPen(false);
+    rotate(90, false);
+    move(linear_speed, 1.5, true);
+    rotate(90, false);
+    drawI();
+
+    setPen(false);
+    rotate(90, false);
+    move(linear_speed, 0.5, true);
+    rotate(90, false);
+    drawN();
+
+    setPen(false);
+    rotate(90, false);
+    move(linear_speed, 0.5, true);
+    rotate(90, false);
+    drawA();
+
+}
+
+void poseCallback(const turtlesim::Pose::ConstPtr &pose_message) {
+    turtlesim_pose.x = pose_message->x;
+    turtlesim_pose.y = pose_message->y;
+    turtlesim_pose.theta = pose_message->theta;
 }
 
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "tina_node");
-    ros::NodeHandle nh;
+    ros::init(argc, argv, "turtlesim_draw_name");
+    ros::NodeHandle n;
 
-    ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
+    velocity_publisher = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
+    pose_subscriber = n.subscribe("/turtle1/pose", 10, poseCallback);
 
-    ros::ServiceClient teleport_client = nh.serviceClient<turtlesim::TeleportAbsolute>("/turtle1/teleport_absolute");
+    ros::Rate loop_rate(0.5);
+    ros::Duration(2).sleep();
 
-    ros::Rate rate(1);
-    
-    rate.sleep(); 
+    executeDrawing();
 
-    setPen(false);
-    teleportTurtle(teleport_client, 2.0, 8.0, 0.0); 
-    setPen(true);
-    drawT(pub);
-
-    setPen(false);
-    teleportTurtle(teleport_client, 5.0, 8.0, 0.0); 
-    setPen(true);
-    drawI(pub);
-
-    setPen(false);
-    teleportTurtle(teleport_client, 7.0, 8.0, 0.0); 
-    setPen(true);
-    drawN(pub);
-
-    setPen(false);
-    teleportTurtle(teleport_client, 10.0, 8.0, 0.0); 
-    setPen(true);
-    drawA(pub);
-
-    ros::spinOnce();
+    ros::spin();
     return 0;
 }
